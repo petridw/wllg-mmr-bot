5adb9290c94d06e1d3dfadbeb34013d055937a91
davidpetri <petridw@gmail.com>
2015-10-01 12:25:22 -0700

    Add cron task which updates all accounts every 15 minutes.

--- index.js
+++ index.js
@@ -5,14 +5,16 @@ var config = require('config');
 var async = require('async');
 var utils = require('./util/helpers');
 var logger = require('./util/logger');
+var moment = require('moment');
+
+var CronJob = require('cron').CronJob;
+var request = require('request');
 
 
 // var steamClient = new Steam.SteamClient();
 var client = new SteamUser();
 var dota2 = new Dota2.Dota2Client(client.client, true);
 var steamClient = client.client;
-
-console.log(dota2.ToSteamID('76180485'));
 
 // Create queue with concurrency of 1 for looking up profiles
 // Keep queue paused until dota2 'ready' event has fired
@@ -74,6 +76,8 @@ dota2.on('ready', function() {
   if (profile_queue.paused) {
     profile_queue.resume();
   }
+  
+  startCron();
 });
 
 dota2.on('unready', function() {
@@ -87,3 +91,67 @@ dota2.on('unready', function() {
 dota2.on('profileData', function(accountId, profileData) {
   logger.info('profileData');
 });
+
+
+function startCron() {
+  var job = new CronJob('00 00,15,30,45 * * * *', function() {
+    logger.info('cron task started');
+    
+    // create queue with concurrency of 1 so we don't make too many requests to steam at once
+    var steam_queue = new async.queue(matchHistory);
+    
+    // Check everyone's accounts
+    request('http://localhost:' + config.get('server').port + '/api/accounts', function(err, response, body) {
+      if (err) return logger.error(err);
+      if (!body) return logger.error('No account list received');
+      
+      var accounts = JSON.parse(body);
+      
+      accounts.forEach(function(account) {
+        steam_queue.push(account);
+      });
+        
+    });
+    
+  }, function() {
+    logger.info('cron task complete at ' + moment());
+  }, true, 'America/Los_Angeles');
+}
+
+function matchHistory(account, done) {
+  
+  var match_history_url = 'https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/' +
+                          '?key=' + config.get('steam').api_key +
+                          '&account_id=' + account.accountID.substring(1);
+  
+  request(match_history_url, function(err, response, body) {
+    
+    if (err) {
+      logger.error(err);
+      return done();
+    }
+    
+    body = JSON.parse(body);
+
+    if (!body || !body.result || !body.result.status) {
+      logger.error('No valid match data received.');
+      return done();
+    }
+    
+    var lastMatch = body.result.matches[0];
+    var lastDate = parseInt(lastMatch.start_time) * 1000;
+    
+    logger.info('********* LAST MATCH DETAIL **********');
+    logger.info(account.username + ' last played at ' + moment(lastDate).format());
+    
+    if (moment(lastDate).isAfter(moment(account.updatedAt))) {
+      logger.info('Updating ' + account.username + ' because they have a new match');
+      
+      profile_queue.push({
+        accountID: parseInt(account.accountID.substring(1)),
+        dota2: dota2
+      });
+    }
+    done();
+  });
+}
